71a89231cf1c932909b1f8149c3aa70584bbe75e
Doug Guthrie <douglas.p.guthrie@gmail.com>
2024-01-06 15:34:54 -0700

    Add some slack functionality

--- main.py
+++ main.py
@@ -1,7 +1,82 @@
-from fastapi import FastAPI
+# stdlib
+import logging
 
-app = FastAPI()
+# third party
+from fastapi import FastAPI, Request
+from slack_bolt.adapter.fastapi.async_handler import AsyncSlackRequestHandler
+from slack_bolt.async_app import AsyncApp
 
-@app.get("/")
+logging.basicConfig(level=logging.DEBUG)
+
+app = AsyncApp()
+app_handler = AsyncSlackRequestHandler(app)
+
+
+# New functionality
+@app.event("app_home_opened")
+async def update_home_tab(client, event, logger):
+    try:
+        # views.publish is the method that your app uses to push a view to the Home tab
+        client.views_publish(
+            # the user that opened your app's app home
+            user_id=event["user"],
+            # the view object that appears in the app home
+            view={
+                "type": "home",
+                "callback_id": "home_view",
+                # body of the view
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "*Welcome to your _App's Home tab_* :tada:",
+                        },
+                    },
+                    {"type": "divider"},
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app.",
+                        },
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {"type": "plain_text", "text": "Click me!"},
+                            }
+                        ],
+                    },
+                ],
+            },
+        )
+
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
+
+@app.event("app_mention")
+async def handle_app_mentions(body, say, logger):
+    logger.info(body)
+    await say("What's up?")
+
+
+@app.event("message")
+async def handle_message():
+    pass
+
+
+api = FastAPI()
+
+
+@api.post("/slack/events")
+async def endpoint(req: Request):
+    return await app_handler.handle(req)
+
+
+@api.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
